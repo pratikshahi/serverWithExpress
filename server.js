67222d1368fa376a6fd0665a706b96dd57a1e896
Pratik Shahi <shahipr0@gmail.com>
2021-03-19 20:22:26 +0530

    created basic server with expressJS

---
+++ server.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+const express=require("express"); //incoprating express to file
+const app=express();                //  express function into app              
+//listen method //3000 is port listening to
+app.listen(3000,function(){
+
+    console.log("server started on port 3000");
+
+
+});                  
